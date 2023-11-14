e9d3f647469781fe7ecab587779f026775c6a6c7
sshk <sashaki02@gmail.com>
2023-11-15 00:04:29 +0300

    week10

--- week10/ex23.sh
+++ week10/ex23.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-bash gen.sh 10 ex1.txt
+bash gen.sh 10 ex23.txt
 
-ln ex1.txt ex11.txt
-ln ex1.txt ex12.txt
+ln ex23.txt ex11.txt
+ln ex23.txt ex12.txt
 
-cat ex1.txt
+cat ex23.txt
 cat ex11.txt
 cat ex12.txt
 
 
-ls -i ex1.txt ex11.txt ex12.txt > output.txt
+ls -i ex23.txt ex11.txt ex12.txt > output.txt
 
-du ex1.txt
+du ex23.txt
 
-ln ex1.txt ex13.txt
+ln ex23.txt ex13.txt
 mv ex13.txt /tmp
 
-find ./ -inum $(ls -i ex1.txt | cut -d' ' -f1)
+find ./ -inum $(ls -i ex23.txt | cut -d' ' -f1)
 
-find / -inum $(ls -i ex1.txt | cut -d' ' -f1) 2>/dev/null
+find / -inum $(ls -i ex23.txt | cut -d' ' -f1) 2>/dev/null
 
 ls -l ex1.txt
 
-find / -inum $(ls -i ex1.txt | cut -d' ' -f1) -exec rm {} \;
+find / -inum $(ls -i ex23.txt | cut -d' ' -f1) -exec rm {} \;
 
